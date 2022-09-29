3377a551f297225f879cbd2a861516856bff5f91
Ilya Polyanskiy <ilya3520041@gmail.com>
2022-09-29 22:58:27 +0300

    comments

--- pervayaproga.py
+++ pervayaproga.py
@@ -1,10 +1,10 @@
-import math
-import numpy
-import matplotlib.pyplot as mpp
-if __name__=='__main__':
-    arguments = numpy.arange(0, 200, 0.1)
-    mpp.plot(
+import math  # импортируем библиотеку чтобы считать синусы
+import numpy  # штука для создания массива для графика
+import matplotlib.pyplot as mpp  # библеотека для построения/вывода графика
+if __name__ == '__main__':  # честно говоря не знаю как это работет
+    arguments = numpy.arange(0, 200, 0.1)  # создаём массив - ось x для графика от 0 до 200 с шагом в 0.1
+    mpp.plot(  # строим график
         arguments,
-        [math.sin(a) * math.sin(a/20.0) for a in arguments]
-    )
-    mpp.show()
+        [math.sin(a) * math.sin(a/20.0) for a in arguments]  # находим значения y для раннее созданного графика
+        )
+    mpp.show()  # выоводим график
