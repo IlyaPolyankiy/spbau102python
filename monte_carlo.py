d01e2e0e8640a463653b40a382ed3a9c53df9aab
Ilya Polyanskiy <ilya3520041@gmail.com>
2023-03-16 20:54:57 +0300

    Считаем пи

---
+++ monte_carlo.py
@@ -0,0 +1,17 @@
+import random
+
+def CountPI(n):
+    count = 0
+    incircle = 0
+    while count < n:
+        # Генерируем координаты точки
+        x = random.random()  
+        y = random.random()
+        # Если квадарт длины (следовательно сама длина) меньше 1, то точка лежит внутри окружности
+        if 1>x*x+y*y:  
+            incircle += 1
+        count += 1
+    PI = 4 * incircle / count  # Мы рассматривали только первую четверть круга
+    return PI
+
+print(CountPI(100000))
